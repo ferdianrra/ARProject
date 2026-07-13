883b7d194b4f0f9f1831067647fad4fec61108ec
Ferdiansyah Annora <ferdianrra@student.gunadarma.ac.id>
2026-07-13 20:50:03 +0700

    Initial Commit

---
+++ ARProjectTests/ARProjectTests.swift
@@ -0,0 +1,19 @@
+//
+//  ARProjectTests.swift
+//  ARProjectTests
+//
+//  Created by Ferdiansyah Annora on 13/07/26.
+//
+
+import Testing
+@testable import ARProject
+
+struct ARProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
